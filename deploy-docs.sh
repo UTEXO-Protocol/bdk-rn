f29bd0f7631cf588856b7549674e17983ed78ac7
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-01-12 13:25:44 -0500

    docs: update script for publishing to gh-pages

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,6 +1,11 @@
-rm -rf ./site/
-mkdocs build
-cd site
+# This script will build the website and push it to the gh-pages branch,
+# publishing it automatically to https://bitcoindevkit.github.io/bdk-rn/.
+
+set -euo pipefail
+
+rm -rf ./site/*
+uv run zensical build
+cd ./site/
 git init .
 git switch --create gh-pages
 git add .
